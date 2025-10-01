950ddcd46674c6e213cfb2ae8ac6accc60a8178a
Vivek <vkmm777@gmail.com>
2025-10-01 12:45:25 +0530

    complemte todo-list

--- projects/host-app/src/app/app.routes.ts
+++ projects/host-app/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { loadRemoteModule } from '@angular-architects/module-federation';
+import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
